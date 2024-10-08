6789d7a1e2969997d3b595d9dbe3b7899cb512d4
Korugan32 <tahacansenel@gmail.com>
2024-10-08 13:55:00 +0300

    Add Colors

--- app/src/main/java/com/korugan/weatherappjetpackcompose/ui/theme/Color.kt
+++ app/src/main/java/com/korugan/weatherappjetpackcompose/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val DarkPurple = Color(0xFF4F00A0)
+val DarkBlue = Color(0xFF020D33)
