e618c6741d85054ac93b3c99d650c8513d93d772
Korugan32 <tahacansenel@gmail.com>
2024-10-08 12:57:05 +0300

    Implementation COIL Library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
 
     implementation("androidx.compose.runtime:runtime-livedata:1.7.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
 }
