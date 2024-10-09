73743a7eabf0c55062ee862ba3634c1b79333bc9
Korugan32 <tahacansenel@gmail.com>
2024-10-09 14:10:00 +0300

    Create Forecastday

---
+++ app/src/main/java/com/korugan/weatherappjetpackcompose/util/api/Forecastday.kt
@@ -0,0 +1,9 @@
+package com.korugan.weatherappjetpackcompose.util.api
+
+data class Forecastday(
+    val astro: Astro,
+    val date: String,
+    val date_epoch: Int,
+    val day: Day,
+    val hour: List<Hour>
+)
