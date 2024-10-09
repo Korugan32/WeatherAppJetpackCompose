f9dd9b4854b37c706471f3f0f8420b8e5c05b9a7
Korugan32 <tahacansenel@gmail.com>
2024-10-09 14:10:39 +0300

    Create LocationX

---
+++ app/src/main/java/com/korugan/weatherappjetpackcompose/util/api/LocationX.kt
@@ -0,0 +1,12 @@
+package com.korugan.weatherappjetpackcompose.util.api
+
+data class LocationX(
+    val country: String,
+    val lat: Double,
+    val localtime: String,
+    val localtime_epoch: Int,
+    val lon: Double,
+    val name: String,
+    val region: String,
+    val tz_id: String
+)
