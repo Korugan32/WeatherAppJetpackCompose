a2536b282e8522c8307cc925cbea695ac6f73600
Korugan32 <tahacansenel@gmail.com>
2024-10-10 18:15:33 +0300

    Add Navigation Library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,5 @@ dependencies {
 
     implementation("androidx.compose.runtime:runtime-livedata:1.7.0")
     implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("androidx.navigation:navigation-compose:2.8.0")
 }
