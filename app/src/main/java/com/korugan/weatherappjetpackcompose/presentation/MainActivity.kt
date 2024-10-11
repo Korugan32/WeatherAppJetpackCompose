926dc62788b4f0644136185605dffffd9569f386
Korugan32 <tahacansenel@gmail.com>
2024-10-11 15:42:04 +0300

    Update MainActivity.kt

--- app/src/main/java/com/korugan/weatherappjetpackcompose/presentation/MainActivity.kt
+++ app/src/main/java/com/korugan/weatherappjetpackcompose/presentation/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.lifecycle.ViewModelProvider
 import com.korugan.weatherappjetpackcompose.ui.theme.WeatherAppJetpackComposeTheme
 import com.korugan.weatherappjetpackcompose.util.navigation.Navigation
 
@@ -12,10 +11,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val weatherViewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
         setContent {
             WeatherAppJetpackComposeTheme {
-            Navigation(weatherViewModel)
+            Navigation()
             }
         }
     }
